6de19c1f714514df0a098a5a54585dad42211b2c
jonas beskow <jonas@furhatrobotics.com>
2022-11-14 10:27:15 +0100

    added data and analysis scripts

---
+++ analysis/stat_exp3.py
@@ -0,0 +1,127 @@
+import sys
+import pandas as pd
+import numpy as np
+from scipy import stats
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+from statsmodels.stats.multicomp import MultiComparison
+
+
+csvf = sys.argv[1]
+df0 = pd.read_csv(csvf)
+systems = ['GDSg1p5','ZE','GDS']
+skiprows = 53
+studyid = '636e41d94efeb2a20ac5e98f'
+skipusers = ['608718e0a8520f7baac735e7']
+datadir = 'zeg_style_comb'
+
+def parse_filename(fn):
+    x,A,B,order = fn.split('/')[1].split('.')[0].split('-')
+    if order =='A':
+        return A,B
+    else:
+        return B,A
+
+subjects = {}
+failed = {}
+data = {'system':[],'score':[]}
+for idx,row in df0.iterrows():
+    if idx<skiprows:
+        continue
+
+    if row['study_id'] != studyid:
+        continue
+
+    if row['subject_id'] in skipusers:
+        #print('skipping user')
+        continue
+
+    #print('start')
+    stim = row['stimulus']
+    resp = row['response']
+    subj = row['subject_id']
+    #print(stim,resp,row['experiment'])
+
+    if not isinstance(stim,str):
+        continue
+
+    if 'attention' in stim:
+        if int(resp) != 4:
+            #print('attention fail:',stim,resp,subj)
+            if subj not in failed:
+                failed[subj] = 1
+            else:
+                failed[subj] += 1
+
+    if datadir in stim:
+        subjects[subj] = True
+        resp = int(resp)
+        #print(idx,stim,resp)
+        L,R = parse_filename(stim)
+        Lscore = 2-resp
+        Rscore = resp-2
+        #print(L,Lscore)
+        #print(R,Rscore)
+        data['system'].append(L)    
+        data['score'].append(Lscore)    
+        data['system'].append(R)    
+        data['score'].append(Rscore)    
+    #        print(parse_filename1(row['stimulus']))
+    #    print(row['stimulus'],row['response'])
+#print(data)
+df = pd.DataFrame.from_dict(data)
+#print(df.to_string())
+print('subjects:',len(subjects.keys()))
+print('responses:',df.shape[0]/2)
+print('failed:')
+ff = [id for id in failed.keys() if failed[id]>=2]
+print(ff)
+
+# MEANS & STD
+
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), stats.sem(a)
+    h = se * stats.t.ppf((1 + confidence) / 2., n-1)
+    return m, h, m-h, m+h
+
+print ('Mean and confidence interval')
+print ('============================')
+print('model\tmean\t95%int')
+for system in systems:
+    data = df[df['system']==system]['score'].tolist()
+    m,h,c0,c1 = mean_confidence_interval(data)
+
+    #print('{}\t{:.3f} +/-{:.3f}'.format(system, m, h))
+    print('{}\t& {:.3f} \pm {:.3f}\t'.format(system, m, h))
+
+
+# ANOVA
+
+def anova_and_tukey(df,depvar,indepvar,catlist):
+    
+    dd = []
+    for type in catlist:
+        vals = df[df[indepvar]==type][depvar].tolist()
+        dd.append(vals)
+        #print(type,len(vals),len(dd))
+
+    f,p = stats.f_oneway(*dd)
+
+
+    print ('One-way ANOVA')
+    print ('=============')
+ 
+    print ('F value:', f)
+    print ('P value:', p, '\n')
+
+    data = df[df[indepvar].isin(catlist)][depvar].to_numpy()
+    groups = df[df[indepvar].isin(catlist)][indepvar].to_numpy()
+    
+    mc = MultiComparison(data,groups,catlist)
+    result = mc.tukeyhsd()
+    print(result)
+
+    
+anova_and_tukey(df,'score','system',systems)
+
